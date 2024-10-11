2aa3d25de8f50312212d33d5ae0f9bd4b34af900
Gabriel Ferreira <57750912+gabrielferreira2626@users.noreply.github.com>
2024-10-11 15:13:22 +0100

    init v1

---
+++ index.js
@@ -0,0 +1,316 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import fs from "fs";
+import path from "path";
+import {fileURLToPath} from "url";
+import {execSync} from "child_process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createDirectoryIfNotExists(directoryPath) {
+ if (!fs.existsSync(directoryPath)) {
+  fs.mkdirSync(directoryPath, {recursive: true});
+ }
+}
+
+function writeFileIfNotExists(filePath, content) {
+ if (!fs.existsSync(filePath)) {
+  fs.writeFileSync(filePath, content, "utf8");
+ }
+}
+
+async function main() {
+ const questions = await inquirer.prompt([
+  {
+   type: "input",
+   name: "widget_name",
+   message: "Widget name:",
+   default: "widget-project",
+  },
+  {
+   type: "confirm",
+   name: "install_tailwind",
+   message: "Do you want to install Tailwind CSS?",
+   default: false,
+  },
+  {
+   type: "confirm",
+   name: "install_fluent_ui",
+   message: "Do you want to install Fluent UI?",
+   default: false,
+  },
+  {
+   type: "confirm",
+   name: "install_fluent_icons",
+   message: "Do you want to install Fluent UI Icons?",
+   default: false,
+  },
+ ]);
+
+ const {widget_name} = questions;
+
+ const projectPath = path.resolve(process.cwd(), widget_name);
+ const srcPath = path.join(projectPath, "src");
+ const assetsPath = path.join(srcPath, "Assets");
+ const previewPath = path.join(assetsPath, "Preview");
+
+ // Criar os diretórios necessários
+ [projectPath, srcPath, assetsPath, previewPath].forEach(createDirectoryIfNotExists);
+
+ const pascalCaseWidgetName = widget_name
+  .split("-")
+  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+  .join("");
+
+ const widgetNameSnakeCase = widget_name
+  .replace(/([a-z])([A-Z])/g, "$1_$2")
+  .replace(/[- ]+/g, "_")
+  .toLowerCase();
+ const widgetNameSeparate = widget_name
+  .replace(/([a-z])([A-Z])/g, "$1 $2")
+  .replace(/[_-]+/g, " ")
+  .replace(/\s+/g, " ")
+  .toLowerCase()
+  .split(" ")
+  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+  .join(" ");
+
+ // Criar arquivos necessários
+ writeFileIfNotExists(
+  path.join(srcPath, "Index.html"),
+  `
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+        <div id="app"></div>
+      </body>
+    </html>`.trim()
+ );
+
+ writeFileIfNotExists(
+  path.join(srcPath, "Index.tsx"),
+  `import { ${pascalCaseWidgetName} } from "./${pascalCaseWidgetName}"; export default ${pascalCaseWidgetName};`
+ );
+
+ writeFileIfNotExists(
+  path.join(srcPath, `${pascalCaseWidgetName}.tsx`),
+  `
+    import React from "react";
+    export const XrmContext = React.createContext<Xrm.XrmStatic | undefined>(undefined);
+    const ${pascalCaseWidgetName}: React.FunctionComponent = () => {
+      return (
+        <div className="w-full h-full bg-white">
+          <div>You are in development mode</div>
+        </div>
+      );
+    };
+    export { ${pascalCaseWidgetName} };
+  `.trim()
+ );
+
+ writeFileIfNotExists(
+  path.join(projectPath, ".babelrc"),
+  `{
+      "presets": [
+        [
+          "@babel/preset-env",
+          {
+            "useBuiltIns": "usage",
+            "corejs": 3,
+            "debug": false
+          }
+        ],
+        "@babel/preset-react",
+        "@babel/preset-typescript"
+      ],
+      "plugins": [
+        [
+          "@babel/plugin-transform-runtime",
+          {
+            "regenerator": true
+          }
+        ],
+        [
+          "@babel/plugin-proposal-class-properties",
+          {
+            "loose": true
+          }
+        ],
+        [
+          "transform-react-remove-prop-types",
+          {
+            "removeImport": true
+          }
+        ]
+      ],
+      "env": {
+        "development": {
+          "sourceMaps": true,
+          "retainLines": true
+        },
+        "test": {
+          "sourceMaps": true,
+          "retainLines": true
+        }
+      }
+    }`
+ );
+
+ writeFileIfNotExists(
+  path.join(projectPath, ".gitignore"),
+  `/dist
+/node_modules`
+ );
+
+ writeFileIfNotExists(
+  path.join(projectPath, "config.js"),
+  `
+    const config = {
+      logicalName: "${widgetNameSnakeCase}",
+      displayName: "${widgetNameSeparate}",
+      version: 1.00,
+      previewImageComponent: "./src/Assets/Preview/",
+    };
+
+    export default config;
+  `.trim()
+ );
+
+ writeFileIfNotExists(
+  path.join(projectPath, "tsconfig.json"),
+  `{
+      "compilerOptions": {
+        "target": "ES5",
+        "module": "ESNext",
+        "jsx": "react",
+        "strict": true,
+        "esModuleInterop": true,
+        "skipLibCheck": true,
+        "forceConsistentCasingInFileNames": true,
+        "moduleResolution": "Node"
+      },
+      "include": ["src/**/*"],
+      "exclude": ["node_modules"]
+    }`
+ );
+
+ writeFileIfNotExists(
+  path.join(projectPath, "webpack.config.js"),
+  `
+    import webpack from "webpack";
+    import ZipPlugin from "zip-webpack-plugin";
+    import CopyWebpackPlugin from "copy-webpack-plugin";
+    import WebpackBar from "webpackbar";
+    import { fileURLToPath } from "url";
+    import path from "path";
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    export default {
+      plugins: [
+        new WebpackBar({ name: "Widget: ${widgetNameSeparate}" }),
+        new webpack.EnvironmentPlugin({
+          "process.env.NODE_ENV": process.env.NODE_ENV,
+        }),
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: "./src/Assets/Preview/",
+              to: "preview/",
+            },
+          ],
+        }),
+        new ZipPlugin({
+          filename: "widget-${widgetNameSnakeCase}.zip",
+          path: "generated",
+        }),
+      ],
+      entry: {
+        main: "./src/Index.tsx",
+      },
+      output: {
+        filename: "${widgetNameSnakeCase}.js",
+        libraryTarget: "commonjs",
+        path: path.resolve(__dirname, "dist"),
+      },
+      resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+      },
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: "ts-loader",
+            },
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: "babel-loader",
+            },
+          },
+          {
+            test: /\.(png|jpe?g|gif|svg)$/i,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  name: "[name].[ext]",
+                  outputPath: "preview/",
+                  publicPath: "preview/",
+                },
+              },
+            ],
+          },
+        ],
+      },
+    };
+  `.trim()
+ );
+
+ writeFileIfNotExists(
+  path.join(projectPath, "package.json"),
+  `{
+      "name": "${widgetNameSeparate}",
+      "version": "1.0.0",
+      "type": "module",
+      "description": "",
+      "private": true,
+      "author": "",
+      "scripts": {
+        "build": "webpack --mode production"
+      },
+      "dependencies": {
+        "react": "^18.0.0"
+      },
+      "devDependencies": {
+        "@babel/core": "^7.0.0",
+        "@babel/preset-env": "^7.0.0",
+        "@babel/preset-react": "^7.0.0",
+        "@babel/preset-typescript": "^7.0.0",
+        "babel-loader": "^8.0.0",
+        "copy-webpack-plugin": "^11.0.0",
+        "file-loader": "^6.0.0",
+        "ts-loader": "^9.0.0",
+        "typescript": "^4.0.0",
+        "webpack": "^5.0.0",
+        "webpack-cli": "^4.0.0",
+        "zip-webpack-plugin": "^5.0.0"
+      }
+    }`
+ );
+}
+
+main().catch((error) => {
+ console.error("Error:", error);
+ process.exit(1);
+});
